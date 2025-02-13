a3c4e59054a66928126893da3a7b4e1b30adbca6
Johan Magnusson <joma2304@student.miun.se>
2025-02-13 12:37:43 +0100

    Lagt till views för böcker

--- Data/BookDbContext.cs
+++ Data/BookDbContext.cs
@@ -6,5 +6,5 @@ namespace BookApp.Data;
 public class BookDbContext : DbContext {
     public BookDbContext(DbContextOptions<BookDbContext> options) : base(options) { }
 
-    public DbSet<BookModel> Schedules { get; set; }
+    public DbSet<BookModel> Books { get; set; }
 }
